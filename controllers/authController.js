7c38e9b224925b5df5d38c14fffaf5de60bb9e26
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-06 18:06:10 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const authController = require('express').Router();
 const { register } = require('../services/userService.js');
+const { parseError } = require('../util/parser.js');
 
 
 
@@ -12,12 +13,31 @@ authController.get('/register', (req, res) => {
 });
 
 authController.post('/register', async (req, res) => {
-    const token = await register(req.body.username, req.body.password);
+    try {
+        if (req.body.username == '' || req.body.password == '') {
+            throw new Error('All fields are required')
+        }
+        if (req.body.password != req.body.repass) {
+            throw new Error('Passwords dont match')
+        }
+        const token = await register(req.body.username, req.body.password);
+        res.cookie('token', token);
+        res.redirect('/auth/register');
 
+    } catch (error) {
+        console.log(error);
+        const errors = parseError(error);
 
-    res.cookie('token', token);
-    res.redirect('/auth/register');
+        //TODO add error display to actual template from assigment
+        res.render('register', {
+            title: 'Register page',
+            errors,
+            body: {
+                username: req.body.username
+            }
+        });
 
+    }
 });
 
 module.exports = authController;
