e428ff6c87c567a11791a4e3b8c54d32204a64d4
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-06 17:28:34 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,23 @@
 const authController = require('express').Router();
+const { register } = require('../services/userService.js');
+
 
 
 authController.get('/register', (req, res) => {
+
+    //TODO replace with actual view by assigment
     res.render('register', {
         title: 'Register Page'
     });
 });
 
 authController.post('/register', async (req, res) => {
-    console.log(req.body);
+    const token = await register(req.body.username, req.body.password);
 
 
+    res.cookie('token', token);
     res.redirect('/auth/register');
+
 });
 
 module.exports = authController;
