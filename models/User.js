b76831f1c22db0b119702f9f9d0f1a0cb231fc8b
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-06 18:06:12 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,9 +2,14 @@ const { Schema, model } = require('mongoose');
 const { options } = require('../controllers/homeController.js');
 
 
-
+//TODO add User properties and validations from assigment
 const userSchema = new Schema({
-    username: { type: String, required: true, unique: true },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [3, "Username must be at least 3 characters long"]
+    },
     hashedPassword: { type: String, required: true },
 });
 
