88043ed800411d6d2bf1f093ef0be2286fa0a6f8
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-06 16:36:26 +0300

    Create userService.js

---
+++ services/userService.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const User = require('../models/User.js');
+
+const JWT_SECRET = 'sfgjdfjdrtrdthdfg';
+async function register(username, password) {
+
+    const existing = await User.findOne({ username }).collation({ locale: 'en', strength: 2 });
+    if (existing) {
+        throw new Error('Username is taken');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        username,
+        hashedPassword
+    });
+    //TODO see asigments if registration creates user session
+    const token = createSession(user);
+
+    return token;
+}
+
+async function login() {
+
+}
+function createSession({ _id, username }) {
+    const payload = {
+        _id,
+        username
+    };
+
+    const token = jwt.sign(payload, JWT_SECRET);
+    return token;
+};
+
+
+function verifyToken() {
+
+}
+
+module.exports = {
+    register,
+    login,
+    verifyToken,
+
+};
