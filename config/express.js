f3fb8699f89871cd717b4b0744d8e4fbed86809a
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-06 17:28:32 +0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
     const hbs = handlebars.create({
@@ -11,4 +12,5 @@ module.exports = (app) => {
 
     app.use('/static', express.static('static'));
     app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
 }
